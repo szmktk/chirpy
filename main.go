c9aa5ad46475e048b3e4261553520b90982b4247
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2024-10-07 22:48:18 +0200

    Add solution for chapter 1, lesson 5: Fileservers

--- main.go
+++ main.go
@@ -10,12 +10,17 @@ import (
 	"log/slog"
 )
 
-const port = 8080
+const (
+	port         = 8080
+	filePathRoot = "."
+)
 
 var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+
 	var server *http.Server
 	server = &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
@@ -24,11 +29,7 @@ func main() {
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  5 * time.Second,
 	}
-	// commented out because the first exercise is about returning 404 response
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintln(w, "OK")
-	// })
 
-	logger.Info("Starting the server", "port", port)
+	logger.Info("Starting the server", "port", port, "server_dir", filePathRoot)
 	log.Fatal(server.ListenAndServe())
 }
