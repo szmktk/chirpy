e09c3a67cc5af58129dc2a1d5df6a83c362dfd2f
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2024-11-11 16:46:09 +0100

    Add solution for chapter 4, lesson 6: The Profane

--- validate_chirp.go
+++ validate_chirp.go
@@ -4,9 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 const maxChirpLength = 140
+
+var forbiddenWords = map[string]bool{
+	"kerfuffle": true,
+	"sharbert":  true,
+	"fornax":    true,
+}
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	type input struct {
@@ -26,5 +33,18 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, map[string]bool{"valid": true})
+	cleanedBody := sanitizeInput(payload.Body)
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"cleaned_body": cleanedBody})
+}
+
+func sanitizeInput(body string) string {
+	var sanitized []string
+	for _, word := range strings.Split(body, " ") {
+		if forbiddenWords[strings.ToLower(word)] {
+			word = "****"
+		}
+		sanitized = append(sanitized, word)
+	}
+	return strings.Join(sanitized, " ")
 }
