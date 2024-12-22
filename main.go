98cb75101f2bb9995625a86f6d8ed40510b3f61c
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2024-12-22 23:51:56 +0100

    Add solution for chapter 5, lesson 3: SQLC

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/szmktk/chirpy/internal/database"
 
 	"log/slog"
 )
@@ -20,11 +25,24 @@ var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, db: dbQueries}
 
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
