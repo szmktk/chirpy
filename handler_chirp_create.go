cd533e9e0ddc126e632c5298076711954a770733
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2024-12-23 17:17:23 +0100

    Add solution for chapter 5, lesson 6: Create Chirp

---
+++ handler_chirp_create.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/szmktk/chirpy/internal/database"
+)
+
+const maxChirpLength = 140
+
+var forbiddenWords = map[string]bool{
+	"kerfuffle": true,
+	"sharbert":  true,
+	"fornax":    true,
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
+	type input struct {
+		Body   string    `json:"body"`
+		UserId uuid.UUID `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	payload := input{}
+	err := decoder.Decode(&payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error decoding JSON body: %s", err))
+		return
+	}
+
+	if len(payload.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleanedBody := sanitizeInput(payload.Body)
+
+	params := database.CreateChirpParams{
+		Body:   cleanedBody,
+		UserID: payload.UserId,
+	}
+	chirp, err := cfg.db.CreateChirp(r.Context(), params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error creating chirp: %s", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	})
+}
+
+func sanitizeInput(body string) string {
+	var sanitized []string
+	for _, word := range strings.Split(body, " ") {
+		if forbiddenWords[strings.ToLower(word)] {
+			word = "****"
+		}
+		sanitized = append(sanitized, word)
+	}
+	return strings.Join(sanitized, " ")
+}
--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func main() {
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
 	mux.HandleFunc("GET /api/healthz", handlerHealth)
 
 	var server *http.Server
